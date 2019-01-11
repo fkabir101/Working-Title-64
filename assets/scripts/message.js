3354b3a684c466c2429a6cfa920f8cf2b4580f98
cjrojas72 <chris@Christians-MacBook-Pro.local>
2019-01-11 11:56:13 -0500

    add animation to msg

--- assets/scripts/message.js
+++ assets/scripts/message.js
@@ -1,3 +1,7 @@
+
+$(document).ready(function(){
+
+//init firebase
 var config = {
   apiKey: "AIzaSyDScvYoCqRZ1ualJJXD17ycRyCl9xs_y6o",
   authDomain: "class-acivities.firebaseapp.com",
@@ -8,28 +12,74 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//firebase database referemce
 database = firebase.database();
 
-var msg = "";
 
+//add event listener for form submit
 $("#message-submit").on("click", function(){
   
-  msg = $("#message-input").val().trim();
+  event.preventDefault();
 
+  //store data from input
+  var msg= {
+    message: $("#message-input").val().trim()
+  };
 
-  database.ref().set({
-    message: msg
-  });
+  console.log(msg);
 
+  //push new message object to database
+  database.ref().push(msg);
 
+  //clear out input field
   $("#message-input").val("");
 });
 
-database.ref().on("value", function(snapshot){
+//add new message to messages display when new message is added
+database.ref().on("child_added", function(snapshot){
 
   console.log("Message: " + snapshot.val().message);
 
-  $(".messages-display").append("<div class='message-div d-inline-flex p-2 mb-4 bg-primary text-white'>" + snapshot.val().message + "</div><br>");
+  //creates new message on page
+  $(".messages-display").append("<div class='message-div p-2 mb-4 bg-primary animated pulse'>" + snapshot.val().message + "</div><br>");
 
 });
 
+//add event listener for for submit with enter key
+$(document).keyup(function(event) {
+  
+
+  if (event.keyCode == 13) {
+    event.preventDefault();
+
+    //if input field is empty give an indicator
+    if($("#message-input").val() === ""){
+      $("#message-input").addClass("border border-danger");
+    }
+    //run if there is user input
+    else{
+
+      $("#message-input").removeClass("border border-danger");
+
+      var msg= {
+      message: $("#message-input").val().trim()
+      };
+  
+      console.log(msg);
+  
+      database.ref().push(msg);
+  
+  
+      $("#message-input").val("");
+  
+      database.ref().on("child_added", function(snapshot){
+  
+        console.log("Message: " + snapshot.val().message);
+  
+        $(".messages-display").append("<div class='message-div p-2 mb-4 bg-primary animated pulse'>" + snapshot.val().message + "</div><br>");
+      });
+    }
+  }
+});
+
+});
